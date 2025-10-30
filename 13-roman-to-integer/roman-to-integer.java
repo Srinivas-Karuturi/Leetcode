e3e5edf6d8e32f80bf1cb78cd941740981c3ce34
KARUTURI DEVI SRINIVAS <23a95a6108@aec.edu.in>
2025-10-30 21:35:18 +0530

    Time: 5 ms (59.34%) | Memory: 44.9 MB (36.47%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,19 +1,40 @@
-import java.util.*;
-class Solution {
-    public int romanToInt(String s) {
-        Map<Character, Integer> map = new HashMap<>(Map.of('I', 1, 'V', 5, 'X', 10,
-            'L', 50, 'C', 100, 'D', 500, 'M', 1000));
-        int res = 0;
-        for(int i=0;i<s.length();i++){
-            int curr = map.get(s.charAt(i));
-            int next = (i+1<s.length()) ? map.get(s.charAt(i+1)) : 0;
-            if(curr < next){
-                res -= curr;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+        
+    	public static int romanToInt(String s) {
+            Map<Character, Integer> roman = new HashMap<>();
+            roman.put('I', 1);
+            roman.put('V', 5);
+            roman.put('X', 10);
+            roman.put('L', 50);
+            roman.put('C', 100);
+            roman.put('D', 500);
+            roman.put('M', 1000);
+
+            int total = 0;
+
+            for (int i = 0; i < s.length(); i++) {
+                int current = roman.get(s.charAt(i));
+                int next = (i+1<s.length()) ? roman.get(s.charAt(i+1)) : 0;
+
+                if (current < next) {
+                    total -= current;
+                } else {
+                    total += current;
+                }
             }
-            else{
-                res += curr;
-            }
+
+            
+
+            return total;
         }
-        return res;
+
+        public static void main(String[] args) {
+            System.out.println(romanToInt("III"));     // 3
+            System.out.println(romanToInt("IX"));      // 9
+            System.out.println(romanToInt("MCMXIV"));   // 58
+            System.out.println(romanToInt("MCMXCIV")); // 1994
+        }
     }
-}
